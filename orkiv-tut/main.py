3f076712c871edc43d73eba4152b72776a96865b
elforzo <erikforss@gmail.com>
2015-02-25 16:01:45 +0100

    Initial commit

---
+++ orkiv-tut/main.py
@@ -0,0 +1,11 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.anchorlayout import AnchorLayout
+
+class AccountDetailsForm(AnchorLayout):
+    pass
+
+class Orkiv(App):
+    pass
+
+Orkiv().run()
